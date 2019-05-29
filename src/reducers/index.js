ce81acc089389bd59e9e392bd2be7f5148ea198b
kunalagg04 <kunalagg04@gmail.com>
2019-05-29 10:06:44 +0530

    Redux setup

---
+++ src/reducers/index.js
@@ -0,0 +1,21 @@
+import { combineReducers } from "redux";
+
+const SongReducer = () => {
+  return [
+    { title: "kaho na kaho", duration: "2:00" },
+    { ttitle: "noha", duration: "3:00" }
+  ];
+};
+
+const SelectedSongReducer = (SelectedSong = null, action) => {
+  if (action.type === "SONG_SELECTED") {
+    return action.payload;
+  }
+
+  return SelectedSong;
+};
+
+export default combineReducers({
+  songs: SongReducer,
+  selectedSong: SelectedSongReducer
+});
